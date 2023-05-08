529d158d3c05e69937cc752949d820d02bc170fb
Alex <alexkachan@protonmail.com>
2023-05-08 03:49:00 -0500

    final steps

--- models/student.js
+++ models/student.js
@@ -14,7 +14,7 @@ module.exports = (sequelize, DataTypes) => {
         }
     })
 
-    Student.sync({force:true}).then( () => {
+    Student.sync({force:false}).then( () => {
         console.log('synced student table')
     })
 
